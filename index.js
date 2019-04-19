62b042aa6438248d2bcd44718661fef4d222532b
Dan Shkolnik <dsmirror28@gmail.com>
2019-04-19 14:26:21 -0400

    scraper with db and express server

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { getTwitterCount, getInstagramCount } from "./lib/scraper";
+import db from "./lib/db";
+
+const app = express();
+
+console.log(db);
+
+app.get("/scrape", async (req, res, next) => {
+  console.log("Scraping!!!");
+
+  const [iCount, tCount] = await Promise.all([
+    getInstagramCount(),
+    getTwitterCount()
+  ]);
+
+  db.get("twitter")
+    .push({
+      date: Date.now(),
+      count: tCount
+    })
+    .write();
+
+  db.get("instagram")
+    .push({
+      date: Date.now(),
+      count: iCount
+    })
+    .write();
+
+  res.json({ iCount, tCount });
+});
+
+app.listen(2093, () => console.log(`Example App running on port 2093`));
